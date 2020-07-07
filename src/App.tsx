694fc796b7cf5505312f93f32764bb7952d89755
endorphin82 <endorphin82@gmail.com>
2020-07-07 19:12:08 +0300

    create product and categories connect

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import 'antd/dist/antd.css'
 import './App.css'
 import { REACT_APP_BASE_URL } from './actions/types'
 import TabsContainer from './components/TabsContainer'
-import categoriesList from './reducer/categories-list'
 
 const App: React.FC = () => {
   console.log('app.tsx')
